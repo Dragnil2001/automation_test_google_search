026a6d9cbeb00bf31197b27cde5b37d33585d33e
Dragnil2001 <zaripov19062016@gmail.com>
2024-07-10 11:28:00 +0300

    Авто тест на поисковике Google (chrome)

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+
+        Configuration.holdBrowserOpen = true;
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+        
+    }
+}
